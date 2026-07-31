2a2dfcf80b81aed4e2ea1b8f3c2a6c59c4a3c6c7
Yunus Emre Gürlek <105203804+Dpehect@users.noreply.github.com>
2026-07-31 04:46:09 +0300

    Add resilient crawler HTTP client and source repository

--- src/TenderScope.Infrastructure/DependencyInjection.cs
+++ src/TenderScope.Infrastructure/DependencyInjection.cs
@@ -11,8 +11,17 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<TenderScopeDbContext>(options => options.UseNpgsql(configuration.GetConnectionString("Postgres")));
+        services.AddDbContext<TenderScopeDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("Postgres"), npgsql => npgsql.EnableRetryOnFailure(5)));
+
         services.AddScoped<ITenderRepository, TenderRepository>();
+        services.AddScoped<ITenderSourceRepository, TenderSourceRepository>();
+        services.AddHttpClient("crawler", client =>
+        {
+            client.Timeout = TimeSpan.FromSeconds(30);
+            client.DefaultRequestHeaders.UserAgent.ParseAdd("TenderScopeBot/1.0 (+https://github.com/Dpehect/tenderscope-platform)");
+            client.DefaultRequestHeaders.Accept.ParseAdd("application/json, application/xml, text/xml, text/html;q=0.9, */*;q=0.5");
+        });
         services.AddSingleton<ITenderSource, DemoTenderSource>();
         return services;
     }
